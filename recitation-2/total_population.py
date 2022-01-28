104f5656822380559abeacb4298fe0bebf206b17
wodeni <wn2155@columbia.edu>
2022-01-28 12:29:09 -0500

    docs: function docstring

--- recitation-2/total_population.py
+++ recitation-2/total_population.py
@@ -1,12 +1,16 @@
 import requests
 
-res = requests.get("https://api.census.gov/data/2019/pep/charagegroups?get=NAME,POP&for=state:*")
-data = res.json()[1:]
-total_population = 0
-for row in data:
-    state_name, population, state_code = row
-    total_population += int(population)
-print(total_population)
-    
+def total_population_21():  
+    """
+    Compute the total population of 2021 using the census API
+    """
+    res = requests.get(
+        "https://api.census.gov/data/2021/pep/population?get=POP_2021,NAME&for=state:*")
+    data = res.json()[1:]
+    total_population = 0
+    for row in data:
+        population, state_name, state_code = row
+        total_population += int(population)
+    return total_population
 
-
+print(total_population_21())
