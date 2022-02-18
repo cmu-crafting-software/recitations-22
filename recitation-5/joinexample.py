6efe390ac5c191d7d277922ebbab55a6ea00424c
kathjordan <55712134+kathjordan@users.noreply.github.com>
2022-02-18 15:04:34 -0500

    In class work Katie and Jack edits

--- recitation-5/joinexample.py
+++ recitation-5/joinexample.py
@@ -7,7 +7,7 @@ import pandas as pd
 
 # Part 1
 readings = pd.read_csv('readings.csv')
-stations = pd.read_csv('stations.csv')
+stations = pd.read_csv('stations.csv').set_index('id')
 sensors = pd.read_csv('sensors.csv')
 
 #print(readings)
@@ -21,29 +21,29 @@ print(readings.columns)
 print(stations.columns)
 
 # Part 3
-joined = readings.join(stations,how='inner',lsuffix=' readings')
+joined = readings.join(stations, on = 'station_id', how='right',lsuffix=' readings')
 print(joined)
 
 # Stretch goal
-inner_join = joined.join(sensors, how = 'inner', lsuffix = ' sensors')
+inner_join = joined.join(sensors, on = 'station_id', how = 'inner', lsuffix = ' sensors')
 
-left_join = readings.join(stations,how='left',lsuffix=' readings')
+left_join = readings.join(stations, on = 'station_id', how = 'left',lsuffix = ' readings')
 left_join = left_join.join(sensors, how = 'left', lsuffix = ' sensors')
 
-right_join = readings.join(stations,how='right',lsuffix=' readings')
+right_join = readings.join(stations, on = 'station_id', how = 'right', lsuffix = ' readings')
 right_join = right_join.join(sensors, how = 'right', lsuffix = ' sensors')
 
-outer_join = readings.join(stations,how='outer',lsuffix=' readings')
+outer_join = readings.join(stations, on = 'station_id', how = 'outer', lsuffix = ' readings')
 outer_join = outer_join.join(sensors, how = 'outer', lsuffix = ' sensors')
 
-print('Inner Join')
-print(inner_join)
+#print('Inner Join')
+#print(inner_join)
 
-print('Left Join')
-print(left_join)
+#print('Left Join')
+#print(left_join)
 
-print('Right Join')
-print(right_join)
+#print('Right Join')
+#print(right_join)
 
-print('Outer Join')
-print(outer_join)
+#print('Outer Join')
+#print(outer_join)
